9eeba105aa4773ef29a425759dd835d166bb2dcd
nudelchef <nudelchef@gmx.de>
2017-10-24 13:53:36 +0200

    d

--- StringCalculator.java
+++ StringCalculator.java
@@ -4,4 +4,41 @@ import java.lang.Math;
 public class StringCalculator
 {
     
+    public static float calculate(String text)
+    {
+        text = text.trim();
+        text = text.replace(" ", "");
+        
+        // / ((3 * 4) / (3 + 5 * 8)) ^ 3
+        
+        // / http://myregexp.com/
+        // / ([(]{1})(.*)([)]{1})
+        
+        
+        
+        for (int i = 0 ; i < text.length() ; i++)
+        {
+            char CHAR = text.charAt(i);
+        }
+        
+        return 0;
+    }
+    
+    protected class MathElement
+    {
+        private String t;
+        
+        
+        public MathElement(String t)
+        {
+            this.t = t;
+        }
+        
+        public float getValue()
+        {
+            return 0;
+        }
+        
+    }
+    
 }
